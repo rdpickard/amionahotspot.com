246eaa6ac2afdf08f1666064f549aa3d789da1b6
Rob Pickard <pickard@Hemera.local>
2020-09-19 12:16:37 -0400

    adding redis backing support the cache ip2location results

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ import json
 import requests
 import ipaddress
 
+import redis
+
 application = flask.Flask(__name__)
 application.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -23,7 +25,8 @@ def index():
         "network_guess": "-",
         "geo_location_guess": "-",
         "isp_guess": "-",
-        "netspeed_guess": "-"
+        "netspeed_guess": "-",
+        "from_cache": False
     }
 
     ip2location_api_key = os.environ.get("ip2location_api_key")
@@ -36,6 +39,12 @@ def index():
         )
         return response
 
+    redis_connection = None
+    if os.environ.get("REDIS_URL", None) is not None:
+        redis_connection = redis.from_url(os.environ.get("REDIS_URL"))
+
+    # Get the IP of the HTTP requester host. This could have gone through proxies/load balancers so look for a
+    # X-Forwarded-For header. Will return a http 500 failure to the requester if the IP is non-routable
     request_ip = flask.request.remote_addr
     if ipaddress.IPv4Address(request_ip).is_private and flask.request.headers.get('X-Forwarded-For', None) is not None:
         request_ip = flask.request.headers['X-Forwarded-For']
@@ -48,36 +57,52 @@ def index():
         )
         return response
 
-    ip2location_response = requests.get(
-        ip2location_api_base_url.format(query_ip=request_ip, api_key=ip2location_api_key))
+    # If there is a redis instance configured, see if the IP is in there
+    if redis_connection is not None:
+        ok_response_json = redis_connection.get(request_ip)
+        if ok_response_json is not None:
+            ok_response_json["from_cache"] = True
 
-    if ip2location_response.status_code == 200 :
-        if ip2location_response.json().get("mobile_brand", "-") != '-':
-            ok_response_json["amionahotspot"] = True
-            ok_response_json["amionahotspot_numeric"] = 1
-            ok_response_json["network_guess"] = ip2location_response.json().get("mobile_brand")
+    if not ok_response_json["from_cache"]:
+        # the default value of "from_cache" is False, so either there is no redis backing or the requested IP wasn't in
+        # the redis backing. Fetch teh geoIP data from the ip2location service
+
+        ip2location_response = requests.get(
+            ip2location_api_base_url.format(query_ip=request_ip, api_key=ip2location_api_key))
+
+        if ip2location_response.status_code == 200 :
+            if ip2location_response.json().get("mobile_brand", "-") != '-':
+                ok_response_json["amionahotspot"] = True
+                ok_response_json["amionahotspot_numeric"] = 1
+                ok_response_json["network_guess"] = ip2location_response.json().get("mobile_brand")
+            else:
+                ok_response_json["amionahotspot_numeric"] = 0
+
+            ok_response_json["geo_location_guess"] = "{}/{}/{}".format(
+                ip2location_response.json().get("country_code", "Country Unavailable"),
+                ip2location_response.json().get("region_name", "Region Unavailable"),
+                ip2location_response.json().get("city_name", "City Unavailable")
+            )
+            ok_response_json["isp_guess"] = ip2location_response.json().get("isp", "ISP Unavailable")
+            ok_response_json["netspeed_guess"] = ip2location_response.json().get("net_speed", "Speed Unavailable")
+
+            # If there is a redis instance configured, add the geoIP data to it for 1 day's time
+            if redis_connection is not None:
+                redis_connection.set(request_ip, ex=86400)
+
         else:
-            ok_response_json["amionahotspot_numeric"] = 0
-
-        ok_response_json["geo_location_guess"] = "{}/{}/{}".format(
-            ip2location_response.json().get("country_code", "Country Unavailable"),
-            ip2location_response.json().get("region_name", "Region Unavailable"),
-            ip2location_response.json().get("city_name", "City Unavailable")
-        )
-        ok_response_json["isp_guess"] = ip2location_response.json().get("isp", "ISP Unavailable")
-        ok_response_json["netspeed_guess"] = ip2location_response.json().get("net_speed", "Speed Unavailable")
+            # Else if the the ip2location query didn't return a 200
+            response = application.response_class(
+                response=json.dumps({"error": "Response status code from ip2location is {}. Expecting 200".format(
+                    ip2location_response.status_code)}),
+                status=500,
+                mimetype='application/json'
+            )
+            return response
 
         response = application.response_class(
             response=json.dumps(ok_response_json),
             status=200,
-            mimetype='application/json'
-        )
-        return response
-
-    else:
-        response = application.response_class(
-            response=json.dumps({"error": "Response status code from ip2location is {}. Expecting 200".format(ip2location_response.status_code)}),
-            status=500,
             mimetype='application/json'
         )
         return response
